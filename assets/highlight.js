cc90371c0c59214f1856dbdd037de41405ef6862
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-03-01 20:24:18 +0100

    rm

--- assets/highlight.js
+++
@@ -1,5 +0,0 @@
-import hljs from 'highlight.js';
-document.querySelectorAll("code").forEach(function(element) {
-    element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-});
-hljs.highlightAll();
