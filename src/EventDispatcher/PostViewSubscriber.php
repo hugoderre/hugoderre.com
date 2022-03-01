f316f374edde8eb9d45ccd18ee7961c60cbe153f
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-03-01 20:23:26 +0100

    event example

--- src/EventDispatcher/PostViewSubscriber.php
+++ src/EventDispatcher/PostViewSubscriber.php
@@ -4,25 +4,41 @@ namespace App\EventDispatcher;
 
 use App\Event\PostViewEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 class PostViewSubscriber implements EventSubscriberInterface
 {
     private $logger;
-    public function __construct(LoggerInterface $logger) {
+    private $mailer;
+
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
+    {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            'post.view' => 'sendMailToAdmin'
+            // 'post.view' => 'sendMailToAdmin'
         ];
     }
 
-    public function sendMailToAdmin(PostViewEvent $post) {
-        $message = 'L\'article "' . $post->getPost()->getTitle() . '" vient d\'être visité';
-        $this->logger->info($message);
+    public function sendMailToAdmin(PostViewEvent $post)
+    {
+        $email = new TemplatedEmail();
+        $email->from(new Address('hdv2symfapp@yopmail.com', 'Hugo Derre Info'))
+            ->to('hugo.d83@outlook.fr')
+            ->text("Un visiteur est en train de voir la page '" . $post->getPost()->getTitle() . "'")
+            ->htmlTemplate('mails/post-view.html.twig')
+            ->context([
+                'post' => $post->getPost()
+            ])
+            ->subject("Visite du post " . $post->getPost()->getTitle() . "'");
+
+        $this->mailer->send($email);
     }
 }
