af8e7ad6cc2c2d29ce6705b79a28b91bc932f11f
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-03-19 15:45:26 +0100

    login / blog / single style

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -37,5 +37,3 @@ class BlogController extends AbstractController
         ]);
     }
 }
-?>
-
--- src/Form/Type/LoginType.php
+++ src/Form/Type/LoginType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,7 +22,7 @@ class LoginType extends AbstractType {
                     'class' => 'form-group',
                 ],
             ])
-            ->add('password', TextType::class, [
+            ->add('password', PasswordType::class, [
                 'label' => 'Mot de passe',
                 'attr'  => [
                     'placeholder' => '',
