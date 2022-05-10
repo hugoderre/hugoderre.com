fd2f0c7622fc31b566b6214cfdc039f130da258d
Hugo Derré <hugo@MacBook-Air-de-Hugo.local>
2022-05-10 16:30:25 +0200

    Add : email property to User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -52,6 +52,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $posts;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
@@ -191,6 +196,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $post->setAuthor(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
 
         return $this;
     }
