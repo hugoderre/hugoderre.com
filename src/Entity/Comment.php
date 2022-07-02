d111ca02aae507b31aab1843549a132af0161398
Hugo Derré <hugo.d83@outlook.fr>
2022-07-02 14:41:43 +0200

    feat: add Comment entity / repo

---
+++ src/Entity/Comment.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $authorName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $authorEmail;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $spamScore;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVisible;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthorName(): ?string
+    {
+        return $this->authorName;
+    }
+
+    public function setAuthorName(string $authorName): self
+    {
+        $this->authorName = $authorName;
+
+        return $this;
+    }
+
+    public function getAuthorEmail(): ?string
+    {
+        return $this->authorEmail;
+    }
+
+    public function setAuthorEmail(string $authorEmail): self
+    {
+        $this->authorEmail = $authorEmail;
+
+        return $this;
+    }
+
+    public function getSpamScore(): ?int
+    {
+        return $this->spamScore;
+    }
+
+    public function setSpamScore(?int $spamScore): self
+    {
+        $this->spamScore = $spamScore;
+
+        return $this;
+    }
+
+    public function isIsVisible(): ?bool
+    {
+        return $this->isVisible;
+    }
+
+    public function setIsVisible(bool $isVisible): self
+    {
+        $this->isVisible = $isVisible;
+
+        return $this;
+    }
+}
