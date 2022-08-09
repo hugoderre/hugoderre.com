a643193a47b6fecf954161f48ef17b8e6454d103
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-08-09 08:11:06 +0200

    Revert "feat: blog (all) define as homepage"
    This reverts commit 7751dbbb792892585afda1a823e8ddb825963dc7.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class KisouijController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/kisouij', name: 'kisouij')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render('kisouij/index.html.twig', [
-            'page' => 'kisouij'
+        return $this->render('home/index.html.twig', [
+            'page' => 'home'
         ]);
     }
 }
