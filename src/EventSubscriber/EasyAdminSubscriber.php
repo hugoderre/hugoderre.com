8bd8a3f3b6118884990e6fbc75befcbc9043884a
Hugo Derre <hugo.d83@outlook.fr>
2022-09-07 15:47:01 +0200

    feat: user can now change his password in backoffice (user detail)

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -3,25 +3,31 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Post;
+use App\Entity\User;
 use DateTimeImmutable;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
     private $slugger;
 
-    public function __construct(SluggerInterface $slugger)
-    {
+    public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $hasher)
+	{
+		$this->hasher = $hasher;
         $this->slugger = $slugger;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            BeforeEntityUpdatedEvent::class => ['preUpdatePost'],
+            BeforeEntityUpdatedEvent::class => [
+				['preUpdatePost'], 
+				['preUpdateUser']
+			]
         ];
     }
 
@@ -37,5 +43,18 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         $entity->setSlug($slug);
 
         $entity->setUpdatedAt(new DateTimeImmutable());
+    }
+
+    public function preUpdateUser(BeforeEntityUpdatedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof User)) {
+            return;
+        }
+
+		if ($password = $entity->getPassword()) {
+        	$entity->setPassword($this->hasher->hashPassword($entity, $password));
+		}
     }
 }
