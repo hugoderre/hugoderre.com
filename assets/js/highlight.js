e26fd7d3a2776ba6635e3ccb68e9cdaaa4af0774
Hugo Derre <hugo.d83@outlook.fr>
2022-11-07 04:25:47 +0100

    fix: wrong html entities remplacement in hljs

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,6 +1,6 @@
 import hljs from 'highlight.js';
 
-document.querySelectorAll( "code" ).forEach( function ( element ) {
-	element.innerHTML = element.innerHTML.replace( /&/g, "&amp;" ).replace( /</g, "&lt;" ).replace( />/g, "&gt;" ).replace( /"/g, "&quot;" ).replace( /'/g, "&#039;" );
-} );
+// document.querySelectorAll( "code" ).forEach( function ( element ) {
+// element.innerHTML = element.innerHTML.replace( /&/g, "&amp;" ).replace( /</g, "&lt;" ).replace( />/g, "&gt;" ).replace( /"/g, "&quot;" ).replace( /'/g, "&#039;" );
+// } );
 hljs.highlightAll();
