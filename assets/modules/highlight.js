d7572508f30dc64187a62faeab3f6708c96b99db
Hugo Derre <hugo.d83@outlook.fr>
2022-11-28 04:45:51 +0100

    fix(optimization): register only needed languages in highlight.js

--- assets/modules/highlight.js
+++ assets/modules/highlight.js
@@ -1,5 +1,10 @@
 // Use CommonJS instead of ES6 modules for theses packages due to async behavior that causes errors 
-const hljs = require( 'highlight.js' );
+const hljs = require( 'highlight.js/lib/core' );
+
+hljs.registerLanguage( 'xml', require( 'highlight.js/lib/languages/xml' ) );
+hljs.registerLanguage( 'css', require( 'highlight.js/lib/languages/css' ) );
+hljs.registerLanguage( 'javascript', require( 'highlight.js/lib/languages/javascript' ) );
+hljs.registerLanguage( 'php', require( 'highlight.js/lib/languages/php' ) );
 
 window.hljs = hljs;
 
