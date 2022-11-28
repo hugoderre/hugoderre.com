b2c95579900fa63364908a61288938700ede03e1
Hugo Derre <hugo.d83@outlook.fr>
2022-11-28 21:11:27 +0100

    feat(hljs): add shell language support

--- assets/modules/highlight.js
+++ assets/modules/highlight.js
@@ -5,6 +5,7 @@ hljs.registerLanguage( 'xml', require( 'highlight.js/lib/languages/xml' ) );
 hljs.registerLanguage( 'css', require( 'highlight.js/lib/languages/css' ) );
 hljs.registerLanguage( 'javascript', require( 'highlight.js/lib/languages/javascript' ) );
 hljs.registerLanguage( 'php', require( 'highlight.js/lib/languages/php' ) );
+hljs.registerLanguage( 'shell', require( 'highlight.js/lib/languages/shell' ) );
 
 window.hljs = hljs;
 
