b223d579bb845e386096bad54af121e926c2a4e9
Hugo Derre <hugo.d83@outlook.fr>
2022-11-28 04:28:35 +0100

    feat|build: load highlightjs module only in post and project pages

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
 	 */
 	.addEntry( 'app', './assets/app.js' )
 	.addEntry( 'admin', './assets/admin.js' )
+	.addEntry( 'hljs', './assets/modules/highlight.js' )
 
 	// enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
 	.enableStimulusBridge( './assets/controllers.json' )
