8d92caf3f1459a230e45a631be9bbba5e8de3916
Hugo Derre <hugo.d83@outlook.fr>
2022-12-14 02:32:09 +0100

    feat(hljs/ckeditor): add JSON to supported languages

--- assets/modules/highlight.js
+++ assets/modules/highlight.js
@@ -4,6 +4,7 @@ const hljs = require( 'highlight.js/lib/core' );
 hljs.registerLanguage( 'xml', require( 'highlight.js/lib/languages/xml' ) );
 hljs.registerLanguage( 'css', require( 'highlight.js/lib/languages/css' ) );
 hljs.registerLanguage( 'javascript', require( 'highlight.js/lib/languages/javascript' ) );
+hljs.registerLanguage( 'json', require( 'highlight.js/lib/languages/json' ) );
 hljs.registerLanguage( 'php', require( 'highlight.js/lib/languages/php' ) );
 hljs.registerLanguage( 'shell', require( 'highlight.js/lib/languages/shell' ) );
 hljs.registerLanguage( 'yaml', require( 'highlight.js/lib/languages/yaml' ) );
