fab4d8573821ff2dec5a1a0dc825457d111a9823
Hugo Derré <hugo.d83@outlook.fr>
2022-12-21 23:10:18 +0100

    build: remove old registered migrations in DB

---
+++ migrations/Version20221221220324.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221221215508 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Removing the Previously Executed Migrations from Production';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DELETE FROM doctrine_migration_versions WHERE version NOT LIKE "%20220926190834" AND version NOT LIKE "%20221221220324"');
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
