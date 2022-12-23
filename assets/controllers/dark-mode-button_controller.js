53da3726bac65e1863690583f52a502befb9dc3c
Hugo Derré <hugo.d83@outlook.fr>
2022-12-23 14:21:32 +0100

    feat: dark mode refactor: convert scss variable to native css variable for more flexibility at run time

--- assets/controllers/dark-mode-button_controller.js
+++ assets/controllers/dark-mode-button_controller.js
@@ -1,7 +1,42 @@
 import { Controller } from 'stimulus';
+import Cookies from 'js-cookie';
 
 export default class extends Controller {
-	toggleDarkMode( event ) {
-		// console.log( event.target )
+	connect() {
+		this.buttonElement = this.element.querySelector( '#dark-mode-switch' );
+		this.isDarkMode = this.getInitialDarkModeState();
+		this.toggleDarkMode();
+	}
+
+	getInitialDarkModeState() {
+		const darkModePreferenceCookie = Cookies.get( 'darkMode' );
+
+		if ( !darkModePreferenceCookie ) {
+			return this.getBrowserPerfersColorScheme();
+		}
+
+		return darkModePreferenceCookie === 'dark';
+	}
+
+	getBrowserPerfersColorScheme() {
+		if ( window.matchMedia && window.matchMedia( '(prefers-color-scheme: dark)' ).matches ) {
+			return true;
+		}
+		return false;
+	}
+
+	darkModeInputEvent() {
+		this.isDarkMode = this.buttonElement.checked;
+		Cookies.set( 'darkMode', this.isDarkMode ? 'dark' : 'light', { expires: 60 } );
+		this.toggleDarkMode();
+	}
+
+	toggleDarkMode() {
+		const body = document.body
+		if ( this.isDarkMode ) {
+			body.dataset.colorScheme = "dark"
+		} else {
+			body.dataset.colorScheme = "light"
+		}
 	}
 }
