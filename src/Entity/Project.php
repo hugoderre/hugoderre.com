8c98147ecfb3826419254e3ba500b6216cf80791
Hugo Derre <hugo.d83@outlook.fr>
2023-01-05 19:38:23 +0100

    refactor: rename some project entity properties

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -13,16 +13,6 @@ use Doctrine\ORM\Mapping as ORM;
 class Project extends AbstractPostType
 {
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $description;
-
-    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $githubUrl;
@@ -52,35 +42,6 @@ class Project extends AbstractPostType
     {
         $this->gallery = new ArrayCollection();
         $this->tags = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
     }
 
     public function getGithubUrl(): ?string
@@ -166,9 +127,4 @@ class Project extends AbstractPostType
 
 		return $this;
 	}
-	
-    public function __toString()
-    {
-        return $this->name;
-    }
 }
