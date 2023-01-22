2c467d2af3f710268cce6aeb892717e898d5b9c0
Hugo Derre <hugo.d83@outlook.fr>
2023-01-22 20:38:24 +0100

    fix: error when using 'change_locale' route from error pages

--- src/Controller/LocaleController.php
+++ src/Controller/LocaleController.php
@@ -3,27 +3,40 @@
 namespace App\Controller;
 
 use App\Helpers\LocaleHelpers;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\RouterInterface;
 
 class LocaleController extends AbstractController
 {
 	#[Route('/change_locale/{locale}', name: 'change_locale', requirements: ['locale' => '%app_locales%'])]
-	public function changeLocale($locale, Request $request, RouterInterface $router, LocaleHelpers $localeHelpers): Response
+	public function changeLocale($locale, Request $request, RouterInterface $router, LocaleHelpers $localeHelpers, LoggerInterface $logger): Response
 	{
 		$request->getSession()->set('_locale', $locale);
 
 		if($referer = $request->headers->get('referer')) {
 			$refererPathInfo = Request::create($referer)->getPathInfo();
-			$refererRouteName = $router->match($refererPathInfo)['_route'];
 
-			if($refererRouteName && $localeHelpers->isLocalizedRoute($refererRouteName)){
-				$refererRouteParams = $router->match($refererPathInfo)['_route_params'] ?? [];
+			try {
+				$refererRouteInfo = $router->match($refererPathInfo);
+			} catch (ResourceNotFoundException $e) {
+				return $this->redirectToRoute('home');
+			}
+
+			if($localeHelpers->isLocalizedRoute($refererRouteInfo['_route'])){
+				$refererRouteParams = $refererRouteInfo['_route_params'] ?? [];
 				$refererRouteParams['_locale'] = $locale;
-				return $this->redirectToRoute($refererRouteName, $refererRouteParams, Response::HTTP_SEE_OTHER);
+
+				try {
+					return $this->redirectToRoute($refererRouteInfo['_route'], $refererRouteParams, Response::HTTP_SEE_OTHER);
+				} catch (\Exception $e) {
+					$logger->error($e->getMessage());
+					return $this->redirectToRoute('home');
+				}
 			}
 		}
 
