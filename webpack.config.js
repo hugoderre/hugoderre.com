f515a8616f96fe67be6ff9d0fc577f49035db66a
Hugo Derre <hugo.d83@outlook.fr>
2023-02-03 18:52:59 +0100

    feat: add AOS lib for elements fade-in/scroll animation | style: "neoctagon" done

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
 	.addEntry( 'app', './assets/app.js' )
 	.addEntry( 'admin', './assets/admin.js' )
 	.addEntry( 'hljs', './assets/modules/highlight.js' )
+	.addEntry( 'aos', './assets/modules/aos.js' )
 
 	// enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
 	.enableStimulusBridge( './assets/controllers.json' )
